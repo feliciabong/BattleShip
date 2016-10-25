164bc8c1990f04cabb4e026f78fa63617af36b40
AppleLau <4308492@students.swinburne.edu.my>
2016-10-25 15:36:04 +0800

    ResultOfAttack.cs

--- Week 6 converted_battleship/converted_battleship/src/Model/ResultOfAttack.cs
+++ Week 6 converted_battleship/converted_battleship/src/Model/ResultOfAttack.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 // using System.Data;
 using System.Diagnostics;
+using SwinGameSDK;
 /// <summary>
 /// The result of an attack.
 /// </summary>
