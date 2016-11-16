7a54fd79cf54ba14a1e8e0ae867184eb40fc3dee
AppleLau <4308492@students.swinburne.edu.my>
2016-11-16 12:00:10 +0800

    BattleshipUnitTesting.cs

---
+++ Week 6 converted_battleship/converted_battleship/src/BattleshipUnitTesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using SwinGameSDK;
+
+
+namespace BattleShips
+{
+	public class BattleshipUnitTesting
+	{
+		[Test()]
+		public void MenuSetuptAction ()
+		{
+			MenuController.PerformSetupMenuAction (1);
+
+			Assert.IsFalse(GameController.AiSetting == AIOption.Easy);  
+			Assert.IsTrue (GameController.AiSetting == AIOption.Medium); 
+			Assert.IsFalse (GameController.AiSetting == AIOption.Hard);
+
+		}
+	}
+}
+
